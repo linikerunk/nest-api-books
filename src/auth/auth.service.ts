65516fdbb24026ed4e5c6bb5878dafb71811bfdc
linikerj <linikerenem@gmail.com>
2022-12-04 01:03:21 -0300

    feat(back) Create dto and classe module about user and bookmark.

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) { }
+
+  signup() {
+    return { msg: 'I have signup' };
+  }
+
+  signin() {
+    return { msg: 'I have signin' };
+  }
+}
